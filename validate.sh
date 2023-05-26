945d64711ee25825bc977dfda162ae4d59c26400
Nic Anderson <nic@nic-a.net>
2023-05-26 03:42:20 -0400

    Super simple validation script

---
+++ validate.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+FAIL=0
+
+for validate_cmd in ffmpeg ffprobe yt-dlp; do
+  if which $validate_cmd > /dev/null 2>&1; then
+    echo "[PASS] ${validate_cmd} is available"
+  else
+    echo "[FAIL] ${validate_cmd} is missing"
+    FAIL=1
+  fi
+done
+
+if [ $FAIL -ne 0 ]; then
+  exit 2
+fi

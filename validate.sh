01c902287c8c769c5bc07bf6659ad8d9028759af
Nic Anderson <nic@nic-a.net>
2023-08-06 23:10:09 -0400

    swap which and command

--- validate.sh
+++ validate.sh
@@ -3,7 +3,7 @@
 FAIL=0
 
 for validate_cmd in ffmpeg ffprobe yt-dlp; do
-  if which $validate_cmd > /dev/null 2>&1; then
+  if command -v $validate_cmd > /dev/null 2>&1; then
     echo "[PASS] ${validate_cmd} is available"
   else
     echo "[FAIL] ${validate_cmd} is missing"
